0093a0736569919cdac801b29f46b24c9ef5c1c8
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-05-19 15:29:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "14050586"
-API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "5465943450"
-SUPPORT_GRP = "LOVE_FEELINGS_WILL1"
-UPDATE_CHNL = "alonegroup121"
-OWNER_USERNAME = "dil_sagar_121"
+API_ID = "24313439"
+API_HASH = "a007ea183e9276803caba61ad093273b"
+BOT_TOKEN = getenv("BOT_TOKEN", "7718680866:AAEpiQUPUDork9_CbHp56AJtJF_kttU6DQM")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://botbaby:botbaby@cluster0.zngip1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = "7290350162"
+SUPPORT_GRP = "infinitygx_bot_support"
+UPDATE_CHNL = "gxinfinity_support"
+OWNER_USERNAME = "Its_tg_seller"
 
